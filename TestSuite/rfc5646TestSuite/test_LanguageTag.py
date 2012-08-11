d97271edc95fd245135dc032f1361d14b086f5b2
Lance Helsten <lanceh@mozy.com>
2012-08-11 12:13:02 -0600

    Addition of tests and validation for RFC 5646.

--- TestSuite/rfc5646TestSuite/test_LanguageTag.py
+++ TestSuite/rfc5646TestSuite/test_LanguageTag.py
@@ -21,29 +21,192 @@ limitations under the License.
 __docformat__ = "reStructuredText en"
 
 import sys
+import locale
 import unittest
 
 from rfc5646 import *
 
 class TestLanguageTag(unittest.TestCase):
+    
+    def test_default(self):
+        lang, region = locale.getlocale()[0].split('_')
+        code = "{0}-{1}".format(lang, region)
+        x = LanguageTag()
+        self.assertEqual(lang, x.language)
+        self.assertIsNone(x.script)
+        self.assertEqual(region, x.region)
+        self.assertEqual([], x.variants)
+        self.assertEqual([], x.extensions)
+        self.assertEqual([], x.privateuse)
+        self.assertEqual(code, str(x))
+        self.assertEqual("LanguageTag('{0}')".format(code), repr(x))
 
-    def test_language(self):
+    def test_simple(self):
         """Make a simple language tag with only the language element."""
         x = LanguageTag("en")
         self.assertEqual("en", x.language)
+        self.assertIsNone(x.script)
+        self.assertIsNone(x.region)
+        self.assertEqual([], x.variants)
+        self.assertEqual([], x.extensions)
+        self.assertEqual([], x.privateuse)
         self.assertEqual("en", str(x))
         self.assertEqual("LanguageTag('en')", repr(x))
 
+        x = LanguageTag("YuE")
+        self.assertEqual("yue", x.language)
+        self.assertEqual("yue", str(x))
+        self.assertEqual("LanguageTag('yue')", repr(x))
+    
+    def test_grandfathered(self):
+        """Test a grandfathered language tag."""
+        x = LanguageTag("i-enochian")
+        self.assertEqual("i-enochian", x.language)
+        self.assertEqual("i-enochian", str(x))
+        self.assertEqual("LanguageTag('i-enochian')", repr(x))
+    
+    def test_extended(self):
+        x = LanguageTag("ZH-CMN-hANS-cn")
+        self.assertEqual("zh", x.language)
+        self.assertEqual("Hans", x.script)
+        self.assertEqual("CN", x.region)
+        self.assertEqual("zh-cmn-Hans-CN", str(x))
+        self.assertEqual("LanguageTag('zh-cmn-Hans-CN')", repr(x))
+    
+    def test_script(self):
+        """Test a language subtag with script subtag."""
+        x = LanguageTag("ZH-hANT")
+        self.assertEqual("zh", x.language)
+        self.assertEqual("Hant", x.script)
+        self.assertEqual("zh-Hant", str(x))
+        self.assertEqual("LanguageTag('zh-Hant')", repr(x))
+    
+    def test_region(self):
+        """Test a language with a script and region subtags."""
+        x = LanguageTag("de-de")
+        self.assertEqual("de", x.language)
+        self.assertEqual("DE", x.region)
+        self.assertEqual("de-DE", str(x))
+        self.assertEqual("LanguageTag('de-DE')", repr(x))
+
+        x = LanguageTag("es-419")
+        self.assertEqual("es", x.language)
+        self.assertEqual("419", x.region)
+        self.assertEqual("es-419", str(x))
+        self.assertEqual("LanguageTag('es-419')", repr(x))
+    
+    def test_script_region(self):
+        """Test a language with region subtag."""
+        x = LanguageTag("sr-Latn-rs")
+        self.assertEqual("sr", x.language)
+        self.assertEqual("Latn", x.script)
+        self.assertEqual("RS", x.region)
+        self.assertEqual("sr-Latn-RS", str(x))
+        self.assertEqual("LanguageTag('sr-Latn-RS')", repr(x))
+    
+    def test_variant(self):
+        """Test a language with a langauge variant."""
+        x = LanguageTag("sl-ROZAJ-BISKE")
+        self.assertEqual("sl", x.language)
+        self.assertEqual(['rozaj', 'biske'], x.variants)
+        self.assertEqual("sl-rozaj-biske", str(x))
+        self.assertEqual("LanguageTag('sl-rozaj-biske')", repr(x))
+    
+    def test_region_variant(self):
+        """Test a language with a region and langauge variant."""
+        x = LanguageTag("de-CH-1901")
+        self.assertEqual("de", x.language)
+        self.assertEqual("CH", x.region)
+        self.assertEqual(['1901'], x.variants)
+        self.assertEqual("de-CH-1901", str(x))
+        self.assertEqual("LanguageTag('de-CH-1901')", repr(x))
+
+        x = LanguageTag("sl-IT-NeDiS")
+        self.assertEqual("sl", x.language)
+        self.assertEqual("IT", x.region)
+        self.assertEqual(['nedis'], x.variants)
+        self.assertEqual("sl-IT-nedis", str(x))
+        self.assertEqual("LanguageTag('sl-IT-nedis')", repr(x))
+    
+    def test_script_region_variant(self):
+        """Test a language with a script, region, and langauge variant."""
+        x = LanguageTag("hy-Latn-IT-arevela")
+        self.assertEqual("hy", x.language)
+        self.assertEqual("Latn", x.script)
+        self.assertEqual("IT", x.region)
+        self.assertEqual(['arevela'], x.variants)
+        self.assertEqual("hy-Latn-IT-arevela", str(x))
+        self.assertEqual("LanguageTag('hy-Latn-IT-arevela')", repr(x))
+    
+    def test_extension(self):
+        """Test a language with a langauge extension."""
+        x = LanguageTag("en-US-u-isLaMcal")
+        self.assertEqual("en", x.language)
+        self.assertEqual("US", x.region)
+        self.assertEqual(['u-islamcal'], x.extensions)
+        self.assertEqual("en-US-u-islamcal", str(x))
+        self.assertEqual("LanguageTag('en-US-u-islamcal')", repr(x))
+        
+        x = LanguageTag("zh-CN-a-myext-x-private")
+        self.assertEqual("zh", x.language)
+        self.assertEqual("CN", x.region)
+        self.assertEqual(['a-myext'], x.extensions)
+        self.assertEqual(["private"], x.privateuse)
+        self.assertEqual("zh-CN-a-myext-x-private", str(x))
+        self.assertEqual("LanguageTag('zh-CN-a-myext-x-private')", repr(x))
+        
+        x = LanguageTag("en-a-myext-b-another")
+        self.assertEqual("en", x.language)
+        self.assertEqual(['a-myext', 'b-another'], x.extensions)
+        self.assertEqual("en-a-myext-b-another", str(x))
+        self.assertEqual("LanguageTag('en-a-myext-b-another')", repr(x))
+    
+    def test_privateuse(self):
+        """Test a private use tag."""
+        x = LanguageTag("de-CH-x-pHonEbk")
+        self.assertEqual("de", x.language)
+        self.assertEqual("CH", x.region)
+        self.assertEqual(["phonebk"], x.privateuse)
+        self.assertEqual("de-CH-x-phonebk", str(x))
+        self.assertEqual("LanguageTag('de-CH-x-phonebk')", repr(x))
+        
+        x = LanguageTag("az-Arab-x-AZE-derbend")
+        self.assertEqual("az", x.language)
+        self.assertEqual("Arab", x.script)
+        self.assertEqual(["aze", "derbend"], x.privateuse)
+        self.assertEqual("az-Arab-x-aze-derbend", str(x))
+        self.assertEqual("LanguageTag('az-Arab-x-aze-derbend')", repr(x))
+    
+    def test_privateuse_registry(self):
+        """Test a private use registry tag."""
+        x = LanguageTag("x-whatever")
+        self.assertEqual("x-whatever", x.language)
+        self.assertIsNone(x.script)
+        self.assertIsNone(x.region)
+        self.assertEqual([], x.variants)
+        self.assertEqual([], x.extensions)
+        self.assertEqual([], x.privateuse)
+        self.assertEqual("x-whatever", str(x))
+        self.assertEqual("LanguageTag('x-whatever')", repr(x))
+        
     def test_complex(self):
         """Make a complex language tax with all elments."""
-        tag = "en-Latn-US-abcde-abcdef-abcdefg-abcdefgh-1aaa-1901-a-e1-b-e1234567-x-1"
+        tag = "en-Latn-US-abcde-abcdef-abcdefg-abcdefgh-1aaa-1901-a-e1-b-e1234567-x-1-abcd-a321bfrc"
         x = LanguageTag(tag)
         self.assertEqual("en", x.language)
         self.assertEqual("Latn", x.script)
         self.assertEqual("US", x.region)
         self.assertEqual(['abcde', 'abcdef', 'abcdefg', 'abcdefgh', '1aaa', '1901'], x.variants)
         self.assertEqual(['a-e1', 'b-e1234567'], x.extensions)
-        self.assertEqual("x-1", x.privateuse)
+        self.assertEqual(["1", "abcd", "a321bfrc"], x.privateuse)
         self.assertEqual(tag, str(x))
         self.assertEqual("LanguageTag('{0}')".format(tag), repr(x))
+    
+    def test_invalid_tworegions(self):
+        self.assertRaises(ValueError, LanguageTag, 'de-419-DE')
+        self.assertRaises(ValueError, LanguageTag, 'a-DE')
+        self.assertRaises(ValueError, LanguageTag, 'ar-a-aaa-b-bbb-a-ccc')
+        self.assertRaises(ValueError, LanguageTag, 'tlh-a-b-foo')
 
+
+        
--- rfc5646/rfc5646.py
+++ rfc5646/rfc5646.py
@@ -20,15 +20,21 @@ limitations under the License.
 import sys
 if sys.version_info < (3, 2):
     raise Exception("Language-Tag requires Python 3.2 or higher.")
+import locale
 import logging
 import string
 import re
+
+from .codevalidation import *
 
 __all__ = ['LanguageTag']
 __log__ = logging.getLogger(__name__)
 
 langtag = re.compile(r'''^
-    (?P<language>[a-zA-Z]{2,3})
+    (?P<language>
+        ([a-zA-Z]{2,3}(?P<extlang>-([a-zA-Z]{3})|(-[a-zA-Z]{3}){0,2})?)|
+        ([a-zA-Z]{4})|
+        ([a-zA-Z]{5,8}))
     (-(?P<script>[a-zA-Z]{4}))?
     (-(?P<region>([a-zA-Z]{2})|([0-9]{3})))?
     (?P<variants>(-(([a-zA-Z0-9]{5,8})|([0-9][a-zA-Z0-9]{3})))*)?
@@ -48,48 +54,117 @@ regular = ["art-lojban", "cel-gaulish", "no-bok", "no-nyn", "zh-guoyu",
 
 
 class LanguageTag():
-    """A language tag as defined in `RFC 5646 Tags for Identifying
-    Languages <http://tools.ietf.org/html/rfc5646>`_
+    """This will parse a `value` that conforms to the language tag
+    ABNF as defined in `RFC 5646 Tags for Identifying Languages
+    <http://tools.ietf.org/html/rfc5646>`_, and will then `validate`
+    the resulting parts against known accepted values.
     
-    Parameter
-    ---------
-    value
-        A string to parse for the `Language-Tag`.
+    If `value` is `None` or an empty string then this will be initialized
+    to `locale.getlocale()`.
+    
+    Notes
+    -----
+    1. This will only consider the first sequence of non-whitespace
+        characters.
+    
+    2. If `value` is not a `str` then it will be converted to `str`
+        using `ascii` decoding.
+    
+    Properties
+    ----------
+    language
+        A 2 or 3 alpha character ISO 639 code, or a 4 alpha character
+        reserved code, or a 5-8 alpha character registered language
+        subtag.
+    
+    extlang
+        A list of up to two 3 alpha character ISO 639 codes.
+    
+    script
+        An ISO 15924 4 alpha character code.
+    
+    region
+        Either ISO 3166-1 two alpha region code or an ISO 3166-1 three
+        digit numeric code (identical to UN M.49 stated in RFC 5646).
+    
+    variants
+        A list of strings of either 5-8 alphanumeric characters or a
+        numeric character followed by 3 alphanumeric characters.
+    
+    extensions
+        A list of strings of with the first being a singleton in the set
+        (0-9, A-W, Y-Z, a-w, y-z) followed by one or more strings of
+        2-8 alphanumeric characters.
+    
+    privateuse
+        A list of strings of 1-8 alphanumeric characters. When output the
+        singleton 'x-' will be prepended.
     """
-    def __init__(self, value):
+    def __init__(self, value=None, validate=True):
+        self.extlang = None
+        self.script = None
+        self.region = None
+        self.variants = None
+        self.extensions = None
+        self.privateuse = None
+        
+        if not value:
+            value = locale.getlocale()[0].replace('_', '-')
+        
+        if isinstance(value, str):
+            value = value.lstrip()
+            value = value.split()[0]
+        elif isinstance(value, bytes) or isinstance(value, bytearray):
+            value = value.lstrip()
+            value = value.split()[0]
+            value = value.decode(encoding='ascii')
+        
         mo = langtag.match(value)
         if mo:
             self.type = 'langtag'
-            self.language = mo.group('language')
+            
+            if not mo.group('extlang'):
+                self.language = mo.group('language')
+            else:
+                l = mo.group('language')
+                e = mo.group('extlang')
+                self.language = l.replace(e, '')
+                self.extlang = e.strip('-').split('-')
+            
             self.script = mo.group('script')
+            
             self.region = mo.group('region')
-            self.variants = []
+            
             if mo.group('variants'):
                 self.variants = mo.group('variants').strip('-').split('-')
-                if len(self.variants) != len(set(self.variants)):
-                    raise ValueError("Variant subtag used more than once: {0}".format(self.variants))
-            self.extensions = []
+
             if mo.group('extensions'):
                 def appendpart(l, part):
                     part = '-'.join(part)
                     if part:
                         l.append(part)                
+
+                extensions = []
                 parts = mo.group('extensions').split('-')
                 part = []
                 for p in parts:
                     if len(p) == 1:
-                        appendpart(self.extensions, part)
+                        appendpart(extensions, part)
                         part = [p]
                     else:
                         part.append(p)
-                appendpart(self.extensions, part)
-            self.privateuse = mo.group('privateuse')
+                appendpart(extensions, part)
+                self.extensions = extensions
+            
+            if mo.group('privateuse'):
+                self.privateuse = mo.group('privateuse').strip('-').split('-')[1:]
+
         elif privateuse.match(value):
             self.type = 'private'
-            self.language = value
+            self.__language = value
         elif value in irregular or value in regular:
             self.type = 'grandfathered'
-            self.language = value
+            self.__language = value
         else:
             raise ValueError("Invalid LanguageTag `{0}`.".format(value))
     
@@ -99,6 +174,8 @@ class LanguageTag():
     def __str__(self):
         if self.type == 'langtag':
             ret = [self.language]
+            if self.extlang:
+                ret.append('-'.join(self.extlang))
             if self.script:
                 ret.append(self.script)
             if self.region:
@@ -108,12 +185,162 @@ class LanguageTag():
             if self.extensions:
                 ret.append('-'.join(self.extensions))
             if self.privateuse:
-                ret.append(self.privateuse)
+                ret.append('x-' + '-'.join(self.privateuse))
             return '-'.join(ret)
         else:
             return self.language
 
     def __repr__(self):
         return "LanguageTag('{0}')".format(str(self))
+    
+    @property
+    def language(self):
+        return self.__language
+    
+    @language.setter
+    def language(self, value):
+        if not value:
+            raise ValueError("Language code cannot be empty.")
+            
+        value = str(value).lower()
+        if not value.isalpha():
+            raise ValueError("Invalid language code `{0}`.".format(value))
+
+        if len(value) == 2 and valid_ISO639_1(value):
+            self.__language = value
+        elif len(value) == 3 and valid_ISO639_2(value):
+            self.__language = value            
+        elif len(value) == 4:
+            self.__language = value
+        elif len(value) in range(5,9):
+            self.__language = value
+        else:
+            raise ValueError("Invalid language code `{0}`.".format(value))
+    
+    @property
+    def extlang(self):
+        return self.__extlang
+    
+    @extlang.setter
+    def extlang(self, value):
+        if not value:
+            value = []
+        elif not isinstance(value, (list, tuple)):
+            value = [value]
+        
+        if len(value) > 2:
+            raise ValueError("Maximum of two extended language codes allowed.")
+        
+        value = [str(v).lower() for v in value]
+        for v in value:
+            if not (v.isalpha() and len(v) == 3):
+                raise ValueError("Invalid extended language code `{0}`.".format(v))
+        self.__extlang = value
+    
+    @property
+    def script(self):
+        return self.__script
+    
+    @script.setter
+    def script(self, value):
+        if not value:
+            self.__script = None
+        else:
+            value = str(value).title()
+            if not (value.isalpha() and len(value) == 4):
+                raise ValueError("Invalid script code `{0}`.".format(value))
+            self.__script = value
+    
+    @property
+    def region(self):
+        return self.__region
+    
+    @region.setter
+    def region(self, value):
+        if not value:
+            self.__region = None
+        else:
+            value = str(value).upper()
+            if len(value) == 2:
+                if not value.isalpha():
+                    raise ValueError("Invalid alpha region code `{0}`.".format(value))
+            elif len(value) == 3:
+                if not value.isnumeric():
+                    raise ValueError("Invalid numeric region code `{0}`.".format(value))
+            else:
+                raise ValueError("Invalid region code `{0}`.".format(value))
+            self.__region = value
+    
+    @property
+    def variants(self):
+        return self.__variants
+    
+    @variants.setter
+    def variants(self, value):
+        if not value:
+            self.__variants = []
+            return
+        
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+        
+        value = [v.lower() for v in value]
+        for v in value:
+            if v[0].isnumeric() and len(v) == 4:
+                continue
+            if v.isalnum() and len(v) in range(5, 9):
+                continue
+            raise ValueError("Invalid language variant code `{0}`.".format(v))
+        
+        if len(value) != len(set(value)):
+            raise ValueError("Variant subtag used more than once: {0}".format(value))
+        self.__variants = value
+    
+    @property
+    def extensions(self):
+        return self.__extensions
+    
+    @extensions.setter
+    def extensions(self, value):
+        if not value:
+            self.__extensions = []
+            return
+            
+        if not isinstance(value, (list, tuple)):
+            value = [str(value)]
+        
+        value = [v.lower() for v in value]
+        singletons = []
+        for v in value:
+            s, x = v.split('-')
+            if not (s.isalnum() and s not in 'xX' and x.isalnum() and len(x) in range(2, 9)):
+                raise ValueError("Invalid language extension code `{0}`.".format(v))
+            if s in singletons:
+                verr = '-'.join(value)
+                raise ValueError("Duplicate extension singleton letter `{0}` in `{1}`.".format(s, verr))
+            else:
+                singletons.append(s)
+        
+        self.__extensions = value
+    
+    @property
+    def privateuse(self):
+        return self.__privateuse
+    
+    @privateuse.setter
+    def privateuse(self, value):
+        if not value:
+            self.__privateuse = []
+            return
+            
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+        
+        value = [v.lower() for v in value]
+        for v in value:
+            if v.isalnum() and len(v) in range(1, 9):
+                continue
+            raise ValueError("Invalid language privateuse code `{0}`.".format(v))
+        self.__privateuse = value
 
 
